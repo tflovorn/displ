60326d097850206c78beb02f1624bb3aae3456e3
Tim Lovorn <tflovorn@austin.utexas.edu>
2017-07-19 16:04:06 -0500

    check mirror symmetry eigenvalues at K, Gamma

--- displ/plot/model_weights_Gamma.py
+++ displ/plot/model_weights_Gamma.py
@@ -8,7 +8,8 @@ from displ.build.build import _get_work, band_path_labels
 from displ.pwscf.parseScf import fermi_from_scf
 from displ.wannier.extractHr import extractHr
 from displ.wannier.bands import Hk_recip
-from displ.plot.model_weights_K import vec_linspace, top_valence_indices
+from displ.plot.model_weights_K import (vec_linspace, top_valence_indices,
+        basis_state_labels, mirror_op)
 
 def _main():
     parser = argparse.ArgumentParser("Plot band structure",
@@ -20,6 +21,9 @@ def _main():
     parser.add_argument("--num_layers", type=int, default=3,
             help="Number of layers (required if group_layer_* options given)")
     args = parser.parse_args()
+
+    if args.num_layers != 3:
+        raise ValueError("mirror check not implemented for num_layers != 3")
 
     work = _get_work(args.subdir, args.prefix)
     wannier_dir = os.path.join(work, "wannier")
@@ -34,10 +38,13 @@ def _main():
     K = np.array([1/3, 1/3, 0.0])
 
     max_K_factor = 0.3
-    num_ks = 100
+    num_ks = 2
 
     ks = vec_linspace(Gamma, max_K_factor*K, num_ks)
     xs = np.linspace(0.0, max_K_factor, num_ks)
+
+    basis = basis_state_labels(args.num_layers)
+    M = mirror_op()
 
     # Assume SOC present and that model has 2*3 X(p) orbitals per layer
     # and 2*5 M(d) in canonical Wannier90 order.
@@ -61,23 +68,39 @@ def _main():
 
     orbital_group = list(itertools.chain(pz_up, pz_dn, dz2_up, dz2_dn))
 
-    num_top_bands = 2
+    num_top_bands = 2*args.num_layers
+
     weights = []
     for i in range(num_top_bands):
         weights.append([])
 
     for k in ks:
-        #print(k)
+        print("k = ", k)
         Hk = Hk_recip(k, Hr)
         Es, U = np.linalg.eigh(Hk)
 
         top = top_valence_indices(E_F, num_top_bands, Es)
-        #print(top, [Es[t] for t in top])
+        print("top valence", top, [Es[t] for t in top])
+
+        mirror_signs = [1, 1, -1, -1, 1, 1]
 
         for i, band in enumerate(top):
-            #for n, v in enumerate(U[:, band]):
+            state = U[:, band]
+
+            mirror_eval = np.dot(state.conjugate().T, np.dot(M, state))[0, 0]
+            print("band {}; mirror <v|M|v> = {}".format(band, mirror_eval))
+            print("mirror deviation elements")
+            Mdev = np.dot(M, state) - mirror_signs[i] * state
+            for n, v in enumerate(Mdev):
+                if abs(v)**2 > 1e-3:
+                    print(n, basis[n], v, abs(v)**2)
+
+            print("band, orb, orb_label, weight, evec comp")
+            #for n, v in enumerate(state):
             #    if abs(v)**2 > 1e-2:
-            #        print(band, n, abs(v)**2, v)
+            #        print(band, n, basis[n], abs(v)**2, v)
+            for n, v in enumerate(state):
+                print(band, n, basis[n], abs(v)**2, v)
 
             total = 0
 
--- displ/plot/model_weights_K.py
+++ displ/plot/model_weights_K.py
@@ -31,6 +31,71 @@ def top_valence_indices(E_F, N, Es):
 
     return [first_above_E_F - i for i in range(1, N+1)]
 
+def basis_state_labels(num_layers):
+    p_orbitals = ["pz", "px", "py"]
+    d_orbitals = ["dz2", "dxz", "dyz", "dx2-y2", "dxy"]
+
+    labels = []
+    for z in range(num_layers):
+        for pos in ["low", "high"]:
+            for orb in p_orbitals:
+                for spin in ["up", "down"]:
+                    labels.append("z_{}_{}_{}_{}".format(z, pos, orb, spin))
+
+    for z in range(num_layers):
+        for orb in d_orbitals:
+            for spin in ["up", "down"]:
+                labels.append("z_{}_{}_{}".format(z, orb, spin))
+
+    return labels
+
+def mirror_op():
+    z_map = [2, 1, 0]
+    p_pos_map = [1, 0]
+    p_orb_sign = [-1, 1, 1]
+    d_orb_sign = [1, -1, -1, 1, 1]
+
+    total_orbitals = 66
+    M = np.zeros([total_orbitals, total_orbitals], dtype=np.complex128)
+    i = 0
+    # p orbitals
+    for z in range(3):
+        for p_pos in range(2):
+            for p_orb in range(3):
+                for spin in range(2):
+                    ip = orbital_pos(z_map[z], "p", p_pos_map[p_pos], p_orb, spin)
+                    M[ip, i] = p_orb_sign[p_orb]
+                    print("p", i, ip, z, p_pos, p_orb, spin)
+                    i += 1
+
+    # d orbitals
+    for z in range(3):
+        for d_orb in range(5):
+            for spin in range(2):
+                ip = orbital_pos(z_map[z], "d", None, d_orb, spin)
+                M[ip, i] = d_orb_sign[d_orb]
+                print("d", i, ip, z, d_orb, spin)
+                i += 1
+
+    assert((np.dot(M, M) == np.eye(M.shape[0])).all())
+    assert((M.conjugate().T == M).all())
+
+    basis = basis_state_labels(3)
+    for ip in range(total_orbitals):
+        for i in range(total_orbitals):
+            if M[ip, i] != 0:
+                print(ip, i, basis[ip], basis[i], M[ip, i].real)
+
+    return M
+
+def orbital_pos(z, p_or_d, p_pos, orb, spin):
+    if p_or_d == "p":
+        z_base = (3*2*2) * z
+        return z_base + (3*2) * p_pos + 2 * orb + spin
+    else:
+        z_base = (3*2*2) * 3 + (5*2) * z
+        return z_base + 2*orb + spin
+
 def _main():
     parser = argparse.ArgumentParser("Plot band structure",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -41,6 +106,9 @@ def _main():
     parser.add_argument("--num_layers", type=int, default=3,
             help="Number of layers (required if group_layer_* options given)")
     args = parser.parse_args()
+
+    if args.num_layers != 3:
+        raise ValueError("mirror check not implemented for num_layers != 3")
 
     work = _get_work(args.subdir, args.prefix)
     wannier_dir = os.path.join(work, "wannier")
@@ -58,6 +126,9 @@ def _main():
 
     ks = vec_linspace(K, reduction_factor*K, num_ks)
     xs = np.linspace(1, reduction_factor, num_ks)
+
+    basis = basis_state_labels(args.num_layers)
+    M = mirror_op()
 
     # Assume SOC present and that model has 2*3 X(p) orbitals per layer
     # and 2*5 M(d) in canonical Wannier90 order.
@@ -88,6 +159,23 @@ def _main():
 
         for i, band in enumerate(top):
             total = 0
+
+            state = U[:, band]
+
+            mirror_eval = np.dot(state.conjugate().T, np.dot(M, state))[0, 0]
+            print("band {}; mirror <v|M|v> = {}".format(band, mirror_eval))
+            print("mirror deviation elements")
+            #Mdev = np.dot(M, state) - mirror_signs[i] * state
+            #for n, v in enumerate(Mdev):
+            #    if abs(v)**2 > 1e-3:
+            #        print(n, basis[n], v, abs(v)**2)
+
+            print("band, orb, orb_label, weight, evec comp")
+            #for n, v in enumerate(state):
+            #    if abs(v)**2 > 1e-2:
+            #        print(band, n, basis[n], abs(v)**2, v)
+            for n, v in enumerate(state):
+                print(band, n, basis[n], abs(v)**2, v)
 
             for z, (n1_up, n2_up, n1_dn, n2_dn) in enumerate(zip(x2y2_up, xy_up, x2y2_dn, xy_dn)):
                 # Appropriate arrangement for K.
