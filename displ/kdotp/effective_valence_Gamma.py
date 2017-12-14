tflovorn/displ

commit 7a9b200f48b22978db06fb9244ce489fb961d473

add effective dielectric constant calculation: top->bottom version well-behaved

displ/kdotp/effective_valence_Gamma.py
@@ -17,25 +17,10 @@ from displ.kdotp.effective_valence_K import (layer_basis_from_dm,
         correction_Hamiltonian_ps, correction_Hamiltonian_mstar_inverses,
         correction_Hamiltonian_PQ, H_kdotp, effective_mass_band)
 
-def _main():
-    np.set_printoptions(threshold=np.inf)
+def make_effective_Hamiltonian_Gamma(subdir, prefix, top_two_only, verbose=False):
+    num_layers = 3
 
-    parser = argparse.ArgumentParser("Plot band structure",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("prefix", type=str,
-            help="Prefix for calculation")
-    parser.add_argument("--subdir", type=str, default=None,
-            help="Subdirectory under work_base where calculation was run")
-    parser.add_argument("--num_layers", type=int, default=3,
-            help="Number of layers")
-    parser.add_argument("--top_two_only", action='store_true',
-            help="Consider only the top two valence states, without layer-projection")
-    args = parser.parse_args()
-
-    if args.num_layers != 3:
-        assert("num_layers != 3 not implemented")
-
-    work = _get_work(args.subdir, args.prefix)
+    work = _get_work(subdir, prefix)
     wannier_dir = os.path.join(work, "wannier")
     scf_path = os.path.join(wannier_dir, "scf.out")
 
@@ -47,8 +32,10 @@ def _main():
     Gamma_cart = np.array([0.0, 0.0, 0.0])
     K_lat = np.array([1/3, 1/3, 0.0])
     K_cart = np.dot(K_lat, R)
-    print(K_cart)
-    print(latVecs)
+
+    if verbose:
+        print(K_cart)
+        print(latVecs)
 
     upto_factor = 0.3
     num_ks = 100
@@ -56,32 +43,34 @@ def _main():
     ks = vec_linspace(Gamma_cart, upto_factor*K_cart, num_ks)
     xs = np.linspace(0.0, upto_factor, num_ks)
 
-    Hr_path = os.path.join(wannier_dir, "{}_hr.dat".format(args.prefix))
+    Hr_path = os.path.join(wannier_dir, "{}_hr.dat".format(prefix))
     Hr = extractHr(Hr_path)
 
-    if args.top_two_only:
-        Pzs = [np.eye(get_total_orbitals(args.num_layers))]
+    if top_two_only:
+        Pzs = [np.eye(get_total_orbitals(num_layers))]
     else:
-        Pzs = get_layer_projections(args.num_layers)
+        Pzs = get_layer_projections(num_layers)
 
     H_TB_Gamma = Hk(Gamma_cart, Hr, latVecs)
     Es, U = np.linalg.eigh(H_TB_Gamma)
 
-    if args.top_two_only:
+    if top_two_only:
         top = top_valence_indices(E_F, 2, Es)
     else:
-        top = top_valence_indices(E_F, 2*args.num_layers, Es)
+        top = top_valence_indices(E_F, 2*num_layers, Es)
 
     top_states = [U[:, [t]] for t in top]
 
     layer_weights, layer_basis = layer_basis_from_dm(top_states, Pzs)
-    print("layer weights")
-    print(layer_weights)
-    print("layer basis")
-    for i, v in enumerate(layer_basis):
-        print("state ", i)
-        for j in range(len(v)):
-            print(j, v[j])
+
+    if verbose:
+        print("layer weights")
+        print(layer_weights)
+        print("layer basis")
+        for i, v in enumerate(layer_basis):
+            print("state ", i)
+            for j in range(len(v)):
+                print(j, v[j])
 
     complement_basis_mat = nullspace(array_with_rows(layer_basis).conjugate())
     complement_basis = []
@@ -89,7 +78,7 @@ def _main():
         v = complement_basis_mat[:, [i]]
         complement_basis.append(v / np.linalg.norm(v))
 
-    assert(len(layer_basis) + len(complement_basis) == 22*args.num_layers)
+    assert(len(layer_basis) + len(complement_basis) == 22*num_layers)
 
     for vl in [v.conjugate().T for v in layer_basis]:
         for vc in complement_basis:
@@ -98,110 +87,166 @@ def _main():
     # 0th order effective Hamiltonian: H(Gamma) in layer basis.
     H_layer_Gamma = layer_Hamiltonian_0th_order(H_TB_Gamma, layer_basis)
 
-    print("H0")
-    print(H_layer_Gamma)
-
     #E_repr = sum([Es[t] for t in top]) / len(top)
     E_repr = Es[top[0]]
     H_correction = correction_Hamiltonian_0th_order(Gamma_cart, Hr, latVecs, E_repr, complement_basis, layer_basis)
-    print("H_correction")
-    print(H_correction)
-    print("H_correction max")
-    print(abs(H_correction).max())
+
+    H0_tot = H_layer_Gamma + H_correction
 
     H_PQ = correction_Hamiltonian_PQ(K_cart, Hr, latVecs, complement_basis, layer_basis)
-    print("H_PQ max")
-    print(abs(H_PQ).max())
 
     # Momentum expectation values <z_{lp}| dH/dk_{c}|_Gamma |z_l>
     ps = layer_Hamiltonian_ps(Gamma_cart, Hr, latVecs, layer_basis)
     
-    print("p")
-    print(ps)
-
-    print("ps max")
-    print(max([abs(x).max() for x in ps]))
-
     ps_correction = correction_Hamiltonian_ps(Gamma_cart, Hr, latVecs, E_repr, complement_basis, layer_basis)
+
+    ps_tot = ps
+    for i, v in enumerate(ps_correction):
+        ps_tot[i] += v
     
-    print("ps correction")
-    print(ps_correction)
-
-    print("ps_correction max")
-    print(max([abs(x).max() for x in ps_correction]))
-
     # Inverse effective masses <z_{lp}| d^2H/dk_{cp}dk_{c}|_Gamma |z_l>
     mstar_invs = layer_Hamiltonian_mstar_inverses(Gamma_cart, Hr, latVecs, layer_basis)
 
-    print("mstar_inv")
-    print(mstar_invs)
-
-    print("mstar_inv max")
-    print(max([abs(v).max() for k, v in mstar_invs.items()]))
-
     mstar_invs_correction_base, mstar_invs_correction_other = correction_Hamiltonian_mstar_inverses(Gamma_cart, Hr, latVecs, E_repr, complement_basis, layer_basis)
 
-    print("mstar_inv_correction_base")
-    print(mstar_invs_correction_base)
+    mstar_inv_tot = mstar_invs
+    for mstar_contrib in [mstar_invs_correction_base, mstar_invs_correction_other]:
+        for k, v in mstar_contrib.items():
+            mstar_inv_tot[k] += v
 
-    print("mstar_inv_correction_base max")
-    print(max([abs(v).max() for k, v in mstar_invs_correction_base.items()]))
+    if verbose:
+        print("H0")
+        print(H_layer_Gamma)
 
-    print("mstar_inv_correction_other")
-    print(mstar_invs_correction_other)
+        print("H_correction")
+        print(H_correction)
+        print("H_correction max")
+        print(abs(H_correction).max())
 
-    print("mstar_inv_correction_other max")
-    print(max([abs(v).max() for k, v in mstar_invs_correction_other.items()]))
+        print("H_PQ max")
+        print(abs(H_PQ).max())
 
-    H_layers = []
-    for k in ks:
-        q = k - Gamma_cart
+        print("p")
+        print(ps)
 
-        H_layers.append(H_kdotp(q, H_layer_Gamma, H_correction, ps,
-                ps_correction, mstar_invs, mstar_invs_correction_base,
-                mstar_invs_correction_other))
+        print("ps max")
+        print(max([abs(x).max() for x in ps]))
 
-    Emks, Umks = [], []
-    for band_index in range(len(layer_basis)):
-        Emks.append([])
-        Umks.append([])
+        print("ps correction")
+        print(ps_correction)
 
-    for k_index, Hk_layers in enumerate(H_layers):
-        Es, U = np.linalg.eigh(Hk_layers)
-        #print(k_index)
-        #print("U", U)
+        print("ps_correction max")
+        print(max([abs(x).max() for x in ps_correction]))
+
+        print("mstar_inv")
+        print(mstar_invs)
+
+        print("mstar_inv max")
+        print(max([abs(v).max() for k, v in mstar_invs.items()]))
+
+        print("mstar_inv_correction_base")
+        print(mstar_invs_correction_base)
+
+        print("mstar_inv_correction_base max")
+        print(max([abs(v).max() for k, v in mstar_invs_correction_base.items()]))
+
+        print("mstar_inv_correction_other")
+        print(mstar_invs_correction_other)
+
+        print("mstar_inv_correction_other max")
+        print(max([abs(v).max() for k, v in mstar_invs_correction_other.items()]))
+
+        # Fit quality plots.
+        H_layers = []
+        for k in ks:
+            q = k - Gamma_cart
+
+            H_layers.append(H_kdotp(q, H_layer_Gamma, H_correction, ps,
+                    ps_correction, mstar_invs, mstar_invs_correction_base,
+                    mstar_invs_correction_other))
+
+        Emks, Umks = [], []
+        for band_index in range(len(layer_basis)):
+            Emks.append([])
+            Umks.append([])
+
+        for k_index, Hk_layers in enumerate(H_layers):
+            Es, U = np.linalg.eigh(Hk_layers)
+            #print(k_index)
+            #print("U", U)
+
+            for band_index in range(len(layer_basis)):
+                Emks[band_index].append(Es)
+                Umks[band_index].append(U)
 
         for band_index in range(len(layer_basis)):
-            Emks[band_index].append(Es)
-            Umks[band_index].append(U)
+            plt.plot(xs, Emks[band_index])
 
-    for band_index in range(len(layer_basis)):
-        plt.plot(xs, Emks[band_index])
+        TB_Emks = []
+        for m in range(len(top)):
+            TB_Emks.append([])
 
-    TB_Emks = []
-    for m in range(len(top)):
-        TB_Emks.append([])
+        for k in ks:
+            this_H_TB_k = Hk(k, Hr, latVecs)
+            this_Es, this_U = np.linalg.eigh(this_H_TB_k)
 
-    for k in ks:
-        this_H_TB_k = Hk(k, Hr, latVecs)
-        this_Es, this_U = np.linalg.eigh(this_H_TB_k)
+            for m, i in enumerate(top):
+                TB_Emks[m].append(this_Es[i])
 
-        for m, i in enumerate(top):
-            TB_Emks[m].append(this_Es[i])
+        for TB_Em in TB_Emks:
+            plt.plot(xs, TB_Em, 'k.')
 
-    for TB_Em in TB_Emks:
-        plt.plot(xs, TB_Em, 'k.')
+        plt.show()
 
-    plt.show()
+        # Effective masses.
+        print("effective mass, top valence band, TB model: m^*_{xx; yy; xy}")
+        print(effective_mass_band(lambda k: Hk(k, Hr, latVecs), Gamma_cart, top[0], alat_Bohr))
 
-    print("effective mass, top valence band, TB model: m^*_{xx; yy; xy}")
-    print(effective_mass_band(lambda k: Hk(k, Hr, latVecs), Gamma_cart, top[0], alat_Bohr))
+        print("effective mass, top valence band, k dot p model: m^*_{xx; yy; xy}")
+        print(effective_mass_band(lambda k: H_kdotp(k - Gamma_cart, H_layer_Gamma,
+                H_correction, ps, ps_correction, mstar_invs,
+                mstar_invs_correction_base, mstar_invs_correction_other),
+                Gamma_cart, len(layer_basis) - 1, alat_Bohr))
 
-    print("effective mass, top valence band, k dot p model: m^*_{xx; yy; xy}")
-    print(effective_mass_band(lambda k: H_kdotp(k - Gamma_cart, H_layer_Gamma,
-            H_correction, ps, ps_correction, mstar_invs,
-            mstar_invs_correction_base, mstar_invs_correction_other),
-            Gamma_cart, len(layer_basis) - 1, alat_Bohr))
+        # Elements contributing to crossover scale.
+        t_Gamma_a = H0_tot[0, 2]
+        t_Gamma_b = H0_tot[0, 3]
+
+        print("t_Gamma_a, t_Gamma_b")
+        print(t_Gamma_a, t_Gamma_b)
+
+        print("norm(t_Gamma_a)")
+        print(abs(t_Gamma_a))
+
+        print("[t_Gamma]_{2, 4}, [t_Gamma]_{2, 5}")
+        print(H0_tot[2, 4], H0_tot[2, 5])
+
+        t_Gamma_direct = H0_tot[0, 4]
+        print("t_Gamma_direct, norm(t_Gamma_direct)")
+        print(t_Gamma_direct, abs(t_Gamma_direct))
+
+        print("E_SL_Gamma")
+        print([H0_tot[i, i] for i in range(6)])
+
+        print("H0_tot")
+        print(H0_tot)
+
+    return H0_tot, ps_tot, mstar_inv_tot
+
+def _main():
+    np.set_printoptions(threshold=np.inf)
+
+    parser = argparse.ArgumentParser("Compute effective Hamiltonian at Gamma for TMD trilayer system with 2H stacking",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("prefix", type=str,
+            help="Prefix for calculation")
+    parser.add_argument("--subdir", type=str, default=None,
+            help="Subdirectory under work_base where calculation was run")
+    parser.add_argument("--top_two_only", action='store_true',
+            help="Consider only the top two valence states, without layer-projection")
+    args = parser.parse_args()
+
+    make_effective_Hamiltonian_Gamma(args.subdir, args.prefix, args.top_two_only, verbose=True)
 
 if __name__ == "__main__":
     _main()

displ/kdotp/effective_valence_K.py
@@ -228,25 +228,10 @@ def effective_mass_band(Hfn, k0, band_index, alat_Bohr):
 
     return mstars
 
-def _main():
-    np.set_printoptions(threshold=np.inf)
+def make_effective_Hamiltonian_K(subdir, prefix, states_from_dm=True, verbose=False):
+    num_layers = 3
 
-    parser = argparse.ArgumentParser("Plot band structure",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("prefix", type=str,
-            help="Prefix for calculation")
-    parser.add_argument("--subdir", type=str, default=None,
-            help="Subdirectory under work_base where calculation was run")
-    parser.add_argument("--num_layers", type=int, default=3,
-            help="Number of layers")
-    parser.add_argument("--states_from_dm", action='store_true',
-            help="Choose states from layer-projected density matrix")
-    args = parser.parse_args()
-
-    if args.num_layers != 3:
-        assert("num_layers != 3 not implemented")
-
-    work = _get_work(args.subdir, args.prefix)
+    work = _get_work(subdir, prefix)
     wannier_dir = os.path.join(work, "wannier")
     scf_path = os.path.join(wannier_dir, "scf.out")
 
@@ -257,8 +242,10 @@ def _main():
 
     K_lat = np.array([1/3, 1/3, 0.0])
     K_cart = np.dot(K_lat, R)
-    print(K_cart)
-    print(latVecs)
+
+    if verbose:
+        print(K_cart)
+        print(latVecs)
 
     reduction_factor = 0.7
     num_ks = 100
@@ -266,27 +253,29 @@ def _main():
     ks = vec_linspace(K_cart, reduction_factor*K_cart, num_ks)
     xs = np.linspace(1, reduction_factor, num_ks)
 
-    Hr_path = os.path.join(wannier_dir, "{}_hr.dat".format(args.prefix))
+    Hr_path = os.path.join(wannier_dir, "{}_hr.dat".format(prefix))
     Hr = extractHr(Hr_path)
 
-    Pzs = get_layer_projections(args.num_layers)
+    Pzs = get_layer_projections(num_layers)
 
     H_TB_K = Hk(K_cart, Hr, latVecs)
     Es, U = np.linalg.eigh(H_TB_K)
 
-    top = top_valence_indices(E_F, 2*args.num_layers, Es)
+    top = top_valence_indices(E_F, 2*num_layers, Es)
 
-    if args.states_from_dm:
+    if states_from_dm:
         top_states = [U[:, [t]] for t in top]
 
         layer_weights, layer_basis = layer_basis_from_dm(top_states, Pzs)
-        print("layer weights")
-        print(layer_weights)
-        print("layer basis")
-        for i, v in enumerate(layer_basis):
-            print("state ", i)
-            for j in range(len(v)):
-                print(j, v[j])
+
+        if verbose:
+            print("layer weights")
+            print(layer_weights)
+            print("layer basis")
+            for i, v in enumerate(layer_basis):
+                print("state ", i)
+                for j in range(len(v)):
+                    print(j, v[j])
     else:
         # Basis states for the effective Hamiltonian:
         # |P_{z = 0} m_0> (m_0 = highest valence state);
@@ -313,7 +302,7 @@ def _main():
         v = complement_basis_mat[:, [i]]
         complement_basis.append(v / np.linalg.norm(v))
 
-    assert(len(layer_basis) + len(complement_basis) == 22*args.num_layers)
+    assert(len(layer_basis) + len(complement_basis) == 22*num_layers)
 
     for vl in [v.conjugate().T for v in layer_basis]:
         for vc in complement_basis:
@@ -322,112 +311,178 @@ def _main():
     # 0th order effective Hamiltonian: H(K) in layer basis.
     H_layer_K = layer_Hamiltonian_0th_order(H_TB_K, layer_basis)
 
-    print("H0")
-    print(H_layer_K)
-
     #E_repr = sum([Es[t] for t in top]) / len(top)
     E_repr = Es[top[0]]
     H_correction = correction_Hamiltonian_0th_order(K_cart, Hr, latVecs, E_repr, complement_basis, layer_basis)
-    print("H_correction")
-    print(H_correction)
-    print("H_correction max")
-    print(abs(H_correction).max())
+
+    H0_tot = H_layer_K + H_correction
 
     H_PQ = correction_Hamiltonian_PQ(K_cart, Hr, latVecs, complement_basis, layer_basis)
-    print("H_PQ max")
-    print(abs(H_PQ).max())
 
     # Momentum expectation values <z_{lp}| dH/dk_{c}|_K |z_l>
     ps = layer_Hamiltonian_ps(K_cart, Hr, latVecs, layer_basis)
-    
-    print("ps")
-    print(ps)
-
-    print("ps max")
-    print(max([abs(x).max() for x in ps]))
 
     ps_correction = correction_Hamiltonian_ps(K_cart, Hr, latVecs, E_repr, complement_basis, layer_basis)
-    
-    print("ps correction")
-    print(ps_correction)
 
-    print("ps_correction max")
-    print(max([abs(x).max() for x in ps_correction]))
+    ps_tot = ps
+    for i, v in enumerate(ps_correction):
+        ps_tot[i] += v
 
     # Inverse effective masses <z_{lp}| d^2H/dk_{cp}dk_{c}|_K |z_l>
     mstar_invs = layer_Hamiltonian_mstar_inverses(K_cart, Hr, latVecs, layer_basis)
 
-    print("mstar_inv")
-    print(mstar_invs)
-
-    print("mstar_inv max")
-    print(max([abs(v).max() for k, v in mstar_invs.items()]))
-
     mstar_invs_correction_base, mstar_invs_correction_other = correction_Hamiltonian_mstar_inverses(K_cart, Hr, latVecs, E_repr, complement_basis, layer_basis)
 
-    print("mstar_inv_correction_base")
-    print(mstar_invs_correction_base)
+    mstar_inv_tot = mstar_invs
+    for mstar_contrib in [mstar_invs_correction_base, mstar_invs_correction_other]:
+        for k, v in mstar_contrib.items():
+            mstar_inv_tot[k] += v
 
-    print("mstar_inv_correction_base max")
-    print(max([abs(v).max() for k, v in mstar_invs_correction_base.items()]))
+    if verbose:
+        print("H0")
+        print(H_layer_K)
 
-    print("mstar_inv_correction_other")
-    print(mstar_invs_correction_other)
+        print("H_correction")
+        print(H_correction)
+        print("H_correction max")
+        print(abs(H_correction).max())
 
-    print("mstar_inv_correction_other max")
-    print(max([abs(v).max() for k, v in mstar_invs_correction_other.items()]))
+        print("H_PQ max")
+        print(abs(H_PQ).max())
 
-    H_layers = []
-    for k in ks:
-        q = k - K_cart
+        print("ps")
+        print(ps)
 
-        H_layers.append(H_kdotp(q, H_layer_K, H_correction, ps, ps_correction,
-                mstar_invs, mstar_invs_correction_base,
-                mstar_invs_correction_other))
+        print("ps max")
+        print(max([abs(x).max() for x in ps]))
 
-    Emks, Umks = [], []
-    for band_index in range(len(layer_basis)):
-        Emks.append([])
-        Umks.append([])
+        print("ps correction")
+        print(ps_correction)
 
-    for k_index, Hk_layers in enumerate(H_layers):
-        Es, U = np.linalg.eigh(Hk_layers)
-        #print(k_index)
-        #print("U", U)
+        print("ps_correction max")
+        print(max([abs(x).max() for x in ps_correction]))
+
+        print("mstar_inv")
+        print(mstar_invs)
+
+        print("mstar_inv max")
+        print(max([abs(v).max() for k, v in mstar_invs.items()]))
+
+        print("mstar_inv_correction_base")
+        print(mstar_invs_correction_base)
+
+        print("mstar_inv_correction_base max")
+        print(max([abs(v).max() for k, v in mstar_invs_correction_base.items()]))
+
+        print("mstar_inv_correction_other")
+        print(mstar_invs_correction_other)
+
+        print("mstar_inv_correction_other max")
+        print(max([abs(v).max() for k, v in mstar_invs_correction_other.items()]))
+
+        # Fit quality plots.
+        H_layers = []
+        for k in ks:
+            q = k - K_cart
+
+            H_layers.append(H_kdotp(q, H_layer_K, H_correction, ps, ps_correction,
+                    mstar_invs, mstar_invs_correction_base,
+                    mstar_invs_correction_other))
+
+        Emks, Umks = [], []
+        for band_index in range(len(layer_basis)):
+            Emks.append([])
+            Umks.append([])
+
+        for k_index, Hk_layers in enumerate(H_layers):
+            Es, U = np.linalg.eigh(Hk_layers)
+            #print(k_index)
+            #print("U", U)
+
+            for band_index in range(len(layer_basis)):
+                Emks[band_index].append(Es)
+                Umks[band_index].append(U)
 
         for band_index in range(len(layer_basis)):
-            Emks[band_index].append(Es)
-            Umks[band_index].append(U)
+            plt.plot(xs, Emks[band_index])
 
-    for band_index in range(len(layer_basis)):
-        plt.plot(xs, Emks[band_index])
+        TB_Emks = []
+        for m in range(len(top)):
+            TB_Emks.append([])
 
-    TB_Emks = []
-    for m in range(len(top)):
-        TB_Emks.append([])
+        for k in ks:
+            this_H_TB_k = Hk(k, Hr, latVecs)
+            this_Es, this_U = np.linalg.eigh(this_H_TB_k)
 
-    for k in ks:
-        this_H_TB_k = Hk(k, Hr, latVecs)
-        this_Es, this_U = np.linalg.eigh(this_H_TB_k)
+            for m, i in enumerate(top):
+                TB_Emks[m].append(this_Es[i])
 
-        for m, i in enumerate(top):
-            TB_Emks[m].append(this_Es[i])
+        for TB_Em in TB_Emks:
+            plt.plot(xs, TB_Em, 'k.')
 
-    for TB_Em in TB_Emks:
-        plt.plot(xs, TB_Em, 'k.')
+        plt.show()
 
-    plt.show()
+        # Effective masses.
+        print("effective mass, top valence band, TB model: m^*_{xx; yy; xy} / m_e")
+        mstar_TB = effective_mass_band(lambda k: Hk(k, Hr, latVecs), K_cart, top[0], alat_Bohr)
+        print(mstar_TB)
 
-    print("effective mass, top valence band, TB model: m^*_{xx; yy; xy} / m_e")
-    mstar_TB = effective_mass_band(lambda k: Hk(k, Hr, latVecs), K_cart, top[0], alat_Bohr)
-    print(mstar_TB)
+        print("effective mass, top valence band, k dot p model: m^*_{xx; yy; xy} / m_e")
+        mstar_kdotp = effective_mass_band(lambda k: H_kdotp(k - K_cart, H_layer_K,
+                H_correction, ps, ps_correction, mstar_invs,
+                mstar_invs_correction_base, mstar_invs_correction_other),
+                K_cart, len(layer_basis) - 1, alat_Bohr)
+        print(mstar_kdotp)
 
-    print("effective mass, top valence band, k dot p model: m^*_{xx; yy; xy} / m_e")
-    mstar_kdotp = effective_mass_band(lambda k: H_kdotp(k - K_cart, H_layer_K,
-            H_correction, ps, ps_correction, mstar_invs,
-            mstar_invs_correction_base, mstar_invs_correction_other),
-            K_cart, len(layer_basis) - 1, alat_Bohr)
-    print(mstar_kdotp)
+        # Elements contributing to crossover scale.
+        t_K = H0_tot[1, 2]
+        t_K_25 = H0_tot[2, 5]
+        Delta_K = H0_tot[1, 1] - H0_tot[2, 2]
+        lambda_SO = H0_tot[1, 1] - H0_tot[0, 0]
+
+        t_K_direct = H0_tot[1, 5]
+
+        print("t_K, Delta_K, lambda_SO")
+        print(t_K, Delta_K, lambda_SO)
+
+        print("t_K_25")
+        print(t_K_25)
+
+        print("norm(t_K), norm(t_K_25)")
+        print(abs(t_K), abs(t_K_25))
+
+        print("t_K_direct, norm(t_K_direct)")
+        print(t_K_direct, abs(t_K_direct))
+
+        print("E_SL_K top")
+        print([H0_tot[i, i] for i in [1, 3, 5]])
+
+        print("E_SL_K bottom")
+        print([H0_tot[i, i] for i in [0, 2, 4]])
+
+        # Elements contributing to effective dielectric constant.
+        print("Layer on-site energy differences:")
+        print("Top group of bands: middle layer - bottom layer; top layer - middle layer")
+        print(H0_tot[3, 3] - H0_tot[1, 1], H0_tot[5, 5] - H0_tot[3, 3])
+        print("Bottom group of bands: middle layer - bottom layer; top layer - middle layer")
+        print(H0_tot[2, 2] - H0_tot[0, 0], H0_tot[4, 4] - H0_tot[2, 2])
+
+    return H0_tot, ps_tot, mstar_inv_tot
+
+def _main():
+    np.set_printoptions(threshold=np.inf)
+
+    parser = argparse.ArgumentParser("Compute effective Hamiltonian at K for TMD trilayer system with 2H stacking",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("prefix", type=str,
+            help="Prefix for calculation")
+    parser.add_argument("--subdir", type=str, default=None,
+            help="Subdirectory under work_base where calculation was run")
+    parser.add_argument("--states_from_dm", action='store_true',
+            help="Choose states from layer-projected density matrix")
+    args = parser.parse_args()
+
+    make_effective_Hamiltonian_K(args.subdir, args.prefix, args.states_from_dm, verbose=True)
 
 if __name__ == "__main__":
     _main()

commit 8f0e345622a24d224ad3b873f9ca9dc772edf7d6

fix ps and mstar reporting in effective_valence_(K, Gamma)
Due to missing explicit copy operations, the returned values from
make_effective_Hamiltonian_(K, Gamma) were correct but the printed
values and band fit were broken in commit 7a9b200. The necessary
copy operations have been added.

Manually verified that the result of kdotp_efield is unchanged.

displ/kdotp/effective_valence_Gamma.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import argparse
+from copy import deepcopy
 import os
 import itertools
 import numpy as np
@@ -105,7 +106,7 @@ def make_effective_Hamiltonian_Gamma(subdir, prefix, top_two_only, verbose=False
     
     ps_correction = correction_Hamiltonian_ps(Gamma_cart, Hr, latVecs, E_repr, complement_basis, layer_basis)
 
-    ps_tot = ps
+    ps_tot = deepcopy(ps)
     for i, v in enumerate(ps_correction):
         ps_tot[i] += v
     
@@ -114,7 +115,7 @@ def make_effective_Hamiltonian_Gamma(subdir, prefix, top_two_only, verbose=False
 
     mstar_invs_correction_base, mstar_invs_correction_other = correction_Hamiltonian_mstar_inverses(Gamma_cart, Hr, latVecs, E_repr, complement_basis, layer_basis)
 
-    mstar_inv_tot = mstar_invs
+    mstar_inv_tot = deepcopy(mstar_invs)
     for mstar_contrib in [mstar_invs_correction_base, mstar_invs_correction_other]:
         for k, v in mstar_contrib.items():
             mstar_inv_tot[k] += v

displ/kdotp/effective_valence_K.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import argparse
+from copy import deepcopy
 import os
 import itertools
 import numpy as np
@@ -330,7 +331,7 @@ def make_effective_Hamiltonian_K(k0_lat, subdir, prefix, get_layer_basis, verbos
 
     ps_correction = correction_Hamiltonian_ps(K_cart, Hr, latVecs, E_repr, complement_basis, layer_basis)
 
-    ps_tot = ps
+    ps_tot = deepcopy(ps)
     for i, v in enumerate(ps_correction):
         ps_tot[i] += v
 
@@ -339,7 +340,7 @@ def make_effective_Hamiltonian_K(k0_lat, subdir, prefix, get_layer_basis, verbos
 
     mstar_invs_correction_base, mstar_invs_correction_other = correction_Hamiltonian_mstar_inverses(K_cart, Hr, latVecs, E_repr, complement_basis, layer_basis)
 
-    mstar_inv_tot = mstar_invs
+    mstar_inv_tot = deepcopy(mstar_invs)
     for mstar_contrib in [mstar_invs_correction_base, mstar_invs_correction_other]:
         for k, v in mstar_contrib.items():
             mstar_inv_tot[k] += v